611d28f819714d340f9526076a369cd42bb278af
Hassan Barbour <barbour.hassan@gmail.com>
2025-02-12 01:21:23 +0300

    First Push

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="col-lg-12 bg-dark">
+      <p></p>
+      <br />
+      <br />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-2 bg-dark"></div>
+    <div class="col-lg-10">
+      <div class="row">
+        <BackDashboard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
